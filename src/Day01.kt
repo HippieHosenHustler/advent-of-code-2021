5dbf4933337705a6a90d34e8f0012bfa9f4911f1
HippieHosenHustler <scharfe.edwin@gmail.com>
2021-12-01 14:11:22 +0100

    part 1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val intList = input.map { it.toInt() }
+        val iterator = intList.listIterator()
+        var counter = 0
+
+
+        while (iterator.hasNext()) {
+            val next = intList[iterator.nextIndex()]
+            println("next:: $next")
+            if (iterator.hasPrevious()) {
+                val previous = intList[iterator.previousIndex()]
+                println("previous:: $previous")
+                if (next > previous) {
+                    counter++
+                }
+            }
+            iterator.next()
+        }
+
+        return counter
     }
 
     fun part2(input: List<String>): Int {
@@ -8,8 +26,8 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
